7147f9deb0964539dde2d31b75af2e760996a90d
Anderson Castro <andersoncastrosantos@gmail.com>
2022-10-12 22:15:58 -0300

    comecando o criar livros

--- src/app/components/views/livro/livro-create/livro-create.component.ts
+++ src/app/components/views/livro/livro-create/livro-create.component.ts
@@ -1,3 +1,6 @@
+import { Livro } from './../livro.model';
+import { ActivatedRoute, Router } from '@angular/router';
+import { LivroService } from './../livro.service';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 
@@ -13,11 +16,32 @@ export class LivroCreateComponent implements OnInit {
   nome_autor = new FormControl('',[Validators.minLength(3)])
   texto = new FormControl('',[Validators.minLength(10)])
 
-  constructor() { }
+  id_cat: String = ''
 
-  ngOnInit(): void {
+  livro: Livro = {
+    id: '',
+    titulo: '',
+    nome_autor: '',
+    texto: ''
   }
 
+  constructor(private service: LivroService, private route: ActivatedRoute, private router: Router) { }
+
+  ngOnInit(): void {
+    this.id_cat = this.route.snapshot.paramMap.get('id_cat')!
+  }
+
+  create(): void {
+    this.service.create(this.livro, this.id_cat).subscribe( 
+      
+    (reposta)=>{
+      this.router.navigate([`categorias/${this.id_cat}/livros`])
+        this.service.mensagem('Livro Castrado com Sucesso!')
+    }, err =>{
+      this.router.navigate([`categorias/${this.id_cat}/livros`])
+      this.service.mensagem('Erro na Castrado do Livro')
+    })
+  }
   /*Metodo reponsavel por mostra os errros na tela.  */
   getMessage(){
     if(this.titulo.invalid){
--- src/app/components/views/livro/livro.service.ts
+++ src/app/components/views/livro/livro.service.ts
@@ -4,6 +4,7 @@ import { Livro } from './livro.model';
 import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable({
   providedIn: 'root'
@@ -12,12 +13,25 @@ export class LivroService {
 
   baseUrl: String = environment.baseUrl;
   
-  constructor( private http: HttpClient ) { }
+  constructor( private http: HttpClient, private _snack: MatSnackBar ) { }
 
   findAllByCategoria(id_cat: String): Observable<Livro[]>{
      const url = `${this.baseUrl}/livros?categoria=${id_cat}`
      return this.http.get<Livro[]>(url)
   }
-  
 
+  create(livro: Livro, id_cat: String): Observable<Livro>{
+    const url = `${this.baseUrl}/livros?categoria=${id_cat}`
+    console.log(url)
+    return this.http.post<Livro>(url, livro)
+  }
+  
+  mensagem(str: String):void {
+    this._snack.open(`${str}`,`OK`,{
+      horizontalPosition: 'end',
+      verticalPosition: 'top',
+      duration: 5000
+
+    })
+  }
 }
